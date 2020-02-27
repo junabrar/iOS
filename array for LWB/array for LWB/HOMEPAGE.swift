e0a308e92b5cee913a8eeeadbbd585cf873d54dc
junabrar <59817536+junabrar@users.noreply.github.com>
2020-02-27 17:17:02 +0600

    Add files via upload
    Simple App with arrays only.

---
+++ array for LWB/array for LWB/HOMEPAGE.swift
@@ -0,0 +1,30 @@
+//
+//  HOMEPAGE.swift
+//  array for LWB
+//
+//  Created by MacBook Air on 27/2/20.
+//  Copyright © 2020 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class HOMEPAGE: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
